6b76952ef5690b9842354bbf1a065fc1b1f4a153
Yash Lekhwani <yashlekhwani@gmail.com>
2019-10-26 23:56:24 -0500

    got cloudinary upload working

--- app/src/main/java/com/example/makeupmymind/ImageProcessor.java
+++ app/src/main/java/com/example/makeupmymind/ImageProcessor.java
@@ -1,39 +1,34 @@
 package com.example.makeupmymind;
 
-import com.cloudinary.Cloudinary;
-import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
-import java.io.IOException;
+import com.cloudinary.utils.ObjectUtils;
+import com.cloudinary.*;
+
 import java.util.HashMap;
 import java.util.Map;
 
-public class ImageProcessor extends AppCompatActivity {
+public class ImageProcessor
+{
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_image_processor);
-        Cloudinary cloudinary = new Cloudinary();
-//        BasicAWSCredentials credentials = new BasicAWSCredentials("AKIATVAIS4CHW4GJ42WI", "kAqGIHqI/fHAr+FTWIUzCWNDIYi9aSngtmnD7t/y");
-//        AmazonS3Client s3Client = new AmazonS3Client(credentials);
-//        AWSMobileClient.getInstance().initialize(this).execute();
-//        String fileName = "test.jpg";
-//        TransferUtility transferUtility =
-//                TransferUtility.builder()
-//                        .context(getApplicationContext())
-//                        .awsConfiguration(AWSMobileClient.getInstance().getConfiguration())
-//                        .s3Client(s3Client)
-//                        .build();
-//        File file = new File(fileName);
-//        transferUtility.upload("bigdashchungus/" + fileName, file);
-        Map<String, String> params = new HashMap<>();
-        params.put("cloud_name", "yashl");
+    static Cloudinary cloudinary = new Cloudinary();
 
-        try {
-//            cloudinary.uploader().unsignedUpload("s3://bigdashchungus.s3.us-east-2.amazonaws.com/test.jpg", "hkupsid3", params);
-            cloudinary.uploader().unsignedUpload("https://upload.wikimedia.org/wikipedia/commons/8/8d/President_Barack_Obama.jpg", "hkupsid3", params);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public static Thread method1()
+    {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try
+                {
+                    Map<String, String> params = new HashMap<>();
+                    params.put("cloud_name", "yashl");
+
+                    cloudinary.uploader().unsignedUpload("https://upload.wikimedia.org/wikipedia/commons/8/8d/President_Barack_Obama.jpg", "hkupsid3", params);
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        });
+        return thread;
     }
 }
