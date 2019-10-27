1c3c615d4d4ad82c408b61d0d78b407659403a78
kaminisaldanha <kaminisaldanha@gmail.com>
2019-10-26 20:11:54 -0500

    current state of voice recognition

--- app/src/main/java/com/example/makeupmymind/CameraActivity.java
+++ app/src/main/java/com/example/makeupmymind/CameraActivity.java
@@ -1,41 +1,114 @@
 package com.example.makeupmymind;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-public class CameraActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class CameraActivity extends AppCompatActivity implements View.OnClickListener {
+    private SpeechRecognizer speech;
+    private Intent recognizer_intent;
+
+    private Button button;
+    private String text;
+    private boolean speechOver = false;
+    static final String TAG = "MyActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
-
-//        Button captureButton = findViewById(R.id.);
-
-//        captureButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//
-//            }
-//        });
+        super.onCreate(savedInstanceState);
+        button = (Button) findViewById(R.id.voiceRecognition);
+        button.setOnClickListener(this);
+        speech = SpeechRecognizer.createSpeechRecognizer(this);
+        speech.setRecognitionListener(new Listener());
+        recognizer_intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
     }
 
-    /** Check if this device has a camera */
-    private boolean checkCameraHardware(Context context) {
-        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
-            // this device has a camera
-            return true;
-        } else {
-            // no camera on this device
-            return false;
+    public void stop() {
+        Log.d(TAG, "stopped listening");
+        speech.stopListening();
+    }
+
+    class Listener implements RecognitionListener {
+        public void onReadyForSpeech(Bundle params)
+        {
+            Log.d(TAG, "onReadyForSpeech");
+        }
+        public void onBeginningOfSpeech()
+        {
+            Log.d(TAG, "onBeginningOfSpeech");
+        }
+        public void onRmsChanged(float rmsdB)
+        {
+            Log.d(TAG, "onRmsChanged");
+        }
+        public void onBufferReceived(byte[] buffer)
+        {
+            Log.d(TAG, "onBufferReceived");
+        }
+        public void onEndOfSpeech()
+        {
+            speechOver = true;
+            Log.d(TAG, "onEndofSpeech");
+        }
+        public void onError(int error)
+        {
+            Log.d(TAG,  "error " +  error);
+            text = "error: " + error;
+        }
+        public void onResults(Bundle results)
+        {
+            String str = new String();
+            Log.d(TAG, "onResults " + results);
+            ArrayList data = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+            for (int i = 0; i < data.size(); i++)
+            {
+                Log.d(TAG, "result " + data.get(i));
+                str += data.get(i);
+            }
+            text = "results: "+String.valueOf(data.size());
+        }
+        public void onPartialResults(Bundle partialResults)
+        {
+            Log.d(TAG, "onPartialResults");
+        }
+        public void onEvent(int eventType, Bundle params)
+        {
+            Log.d(TAG, "onEvent " + eventType);
         }
     }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.voiceRecognition)
+        {
+            Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+            speech.startListening(intent);
+        }
+    }
+    //    /** Check if this device has a camera */
+//    private boolean checkCameraHardware(Context context) {
+//        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+//            // this device has a camera
+//            return true;
+//        } else {
+//            // no camera on this device
+//            return false;
+//        }
+//    }
 }
 
 
